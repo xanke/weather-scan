cec3703ec72fea8e1a79f3b00b95649e5a9df898
xank <xank@qq.com>
2018-12-19 22:37:46 +0800

    1

--- app/controller/home.ts
+++ app/controller/home.ts
@@ -4,7 +4,7 @@ export default class HomeController extends Controller {
   public async index() {
     const { ctx } = this
     
-    await ctx.model.Forecast.sync({ force: true })
+    // await ctx.model.Forecast.sync({ force: true })
     ctx.body = await ctx.service.test.sayHi('egg')
   }
 }
